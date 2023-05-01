e4e1d0f49daebc03181c160d7ae4b716129f8c63
AlexMarutyan <alex@m.scylla.ai>
2023-05-01 15:46:16 +0400

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import app from "./app.module.scss";
+import Slider from "../src/components/Slider";
+import { Text } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={app.container}>
+      <div className={app.background}>
+        <div className={app.wrapper}>
+          <Text mb="20px" fontSize="24px" fontWeight="700">
+            Long-form
+          </Text>
+          <Text
+            mb={2}
+            w="80%"
+            color="#705cf6"
+            fontSize="16px"
+            fontWeight="bold"
+          >
+            For bloggers, freelancers & businesses
+          </Text>
+          <Text
+            px={2}
+            w="70%"
+            color="#705cf6"
+            fontSize="14px"
+            minHeight="65px"
+            fontWeight="500"
+          >
+            Awesome tools to help you write blog posts, books, and more.
+          </Text>
+          <Slider />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,129 @@
+import { Text } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+
+import styles from "./styles.module.scss";
+
+const prices = [
+  { price: "12.67", users: "1", words: "60,000" },
+  { price: "32.67", users: "5", words: "200,000" },
+  { price: "66", users: "5", words: "400,000" },
+  { price: "132.67", users: "10", words: "800,000" },
+  { price: "332.67", users: "12", words: "2,000,000" },
+  { price: "666", users: "15", words: "4,000,000" },
+];
+
+const Slider = () => {
+  const thumb = useRef();
+  const level = useRef();
+  const [price, setPrice] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleMouseEnter = () => {
+    thumb.current.style.transition = "";
+    level.current.style.transition = "";
+  };
+
+  const handleMouseUp = (event) => {
+    const currentValue = event.target.value;
+    const currentPosition = (currentValue / 20).toString();
+    const numbersArray = currentPosition.split(".");
+
+    thumb.current.style.transition = "left  linear 0.3s";
+    level.current.style.transition = "width  linear 0.3s";
+
+    if (!!numbersArray[1]) {
+      if (numbersArray[1][0] < 5) {
+        setValue(numbersArray[0] * 20);
+      } else {
+        setValue((+numbersArray[0] + 1) * 20);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (Math.round(value / 20) !== price) {
+      setPrice(Math.round(value / 20));
+    }
+  }, [value]);
+
+  return (
+    <>
+      <Text
+        gap={1}
+        mb="50px"
+        display="flex"
+        fontWeight="700"
+        fontSize="44px"
+        alignItems="center"
+      >
+        {`$${prices[price]?.price}`}
+        <Text as="span" fontSize="14px" opacity={0.5} fontWeight="normal">
+          /month
+        </Text>
+      </Text>
+      <div className={styles.slider}>
+        <div className={styles.track}>
+          <div className={styles.min}>60,000</div>
+          <div className={styles.max}>4,000,000</div>
+          {Array(6)
+            .fill(null)
+            .map((_, index) => (
+              <div
+                key={index}
+                className={`${styles?.position} ${
+                  value >= index * 20 ? styles[`pos_${index + 1}_active`] : ""
+                }`}
+                {...(index && { style: { left: `${index * 20}%` } })}
+              />
+            ))}
+          <div
+            ref={thumb}
+            className={styles.thumb}
+            style={{ left: `${value}%` }}
+          />
+          <div
+            ref={level}
+            className={styles.level}
+            style={{ width: `${value}%` }}
+          />
+        </div>
+        <input
+          min="0"
+          max="100"
+          step="any"
+          type="range"
+          value={value}
+          className={styles.input}
+          onMouseUp={handleMouseUp}
+          onChange={handleInputChange}
+          onMouseEnter={handleMouseEnter}
+        />
+      </div>
+      <Text
+        gap={2}
+        display="flex"
+        fontSize="18px"
+        fontWeight="500"
+        alignItems="center"
+      >
+        {prices[price].words} words
+        <Text
+          as="span"
+          bg="#e5e7eb"
+          boxSize="3px"
+          display="block"
+          borderRadius="full"
+        />
+        <Text as="span" fontSize="16px" opacity={0.5} fontWeight="normal">
+          {prices[price].users} users
+        </Text>
+      </Text>
+    </>
+  );
+};
+
+export default Slider;
